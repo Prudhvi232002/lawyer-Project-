da5e5a82c2e0007ecc0a690986fb07ef922574fc
Prudhvi232002 <prudhvi@example.com>
2026-02-23 12:00:04 +0530

    Delete all files - empty repository

--- resources/js/independant_components.js
+++
@@ -1,9 +0,0 @@
-import './bootstrap';
- import { createApp } from 'vue'
-import IndependentComponents from './IndependentComponents.vue'
-
-
-const ind_app = createApp(IndependentComponents)
-ind_app.component('AboutUs',AboutUs)
-ind_app.component('ContactUs',ContactUs)
-ind_app.mount('#ind_app')
